df4d23c71cdee1ce796da73497eeb705e92c5adf
alator-ta <alan@torresatlantic.com>
2026-07-02 10:32:20 -0400

    Update formula to v1.1.0
    - Bump version and point the darwin arm64/amd64 URLs and sha256 checksums at the v1.1.0 release archives.

--- multikey.rb
+++ multikey.rb
@@ -4,16 +4,16 @@
 class Multikey < Formula
     desc "Manage multiple GitHub SSH identities based on folder/repo location"
     homepage "https://github.com/Torres-Atlantic/multikey-cli"
-    version "1.0.0"
+    version "1.1.0"
     license "MIT"
   
     on_macos do
       if Hardware::CPU.arm?
-        url "https://github.com/Torres-Atlantic/multikey-cli/releases/download/v1.0.0/multikey-darwin-arm64.tar.gz"
-        sha256 "a42f2b7d0f71ef600941cb47bf17eba66cb216b25d9d215dca932ee209019c29"
+        url "https://github.com/Torres-Atlantic/multikey-cli/releases/download/v1.1.0/multikey-darwin-arm64.tar.gz"
+        sha256 "d98d21d0e3c39b1f5e6e1ab00d190aa020bc1e3df088f6e65efe3555dd1ecf1a"
       else
-        url "https://github.com/Torres-Atlantic/multikey-cli/releases/download/v1.0.0/multikey-darwin-amd64.tar.gz"
-        sha256 "1f8e1aa86e2154c38625c2687af6f94e89b899908ea95896d096349c211c0fb2"
+        url "https://github.com/Torres-Atlantic/multikey-cli/releases/download/v1.1.0/multikey-darwin-amd64.tar.gz"
+        sha256 "416c9ca2e3353f6466f19efb8e6066a471ad2ee4ac5171e3aec6647595d8ca5f"
       end
     end
   
